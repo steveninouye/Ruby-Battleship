922f0fd3898da32f26a289bce82311e6425f02c8
Steven Inouye <steveninouye@msn.com>
2018-09-15 11:01:27 -1000

    making UI a little nicer

--- lib/player.rb
+++ lib/player.rb
@@ -3,16 +3,18 @@ class HumanPlayer
     
     def self.get_play
         target = []
-        while true
-            input = gets.chomp.split(",").map do 
-            break if target.length == 2
-            p "Input valid target"
+        valid_input = false
+        until valid_input
+            p "Target a coordinate"
+            p "('3 4' => targets row 3 column 4)"
+            target = input_to_integer_arr(gets.chomp)
+            valid_input = input_is_valid?(target)
         end
         target
     end
 
     def input_to_integer_arr(input)
-        num_arr = input.split(",").map do |el|  
+        num_arr = input.split.map do |el|  
             num = el.to_i
             num if el == "0" || num > 0
             # if input is not a number, it will return nil for the index
