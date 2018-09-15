5a5e2acc0817055d4004f959f951d9b4e68e3981
Steven Inouye <steveninouye@msn.com>
2018-09-15 10:48:26 -1000

    completed validating input

--- lib/player.rb
+++ lib/player.rb
@@ -4,21 +4,27 @@ class HumanPlayer
     def self.get_play
         target = []
         while true
-            input = gets.chomp.split(",").map do |el|  
-                if el == "0"
-                    0
-                elsif el.to_i > 0
-                    el.to_i
-                end
-            end
-            target = input.compact
+            input = gets.chomp.split(",").map do 
             break if target.length == 2
             p "Input valid target"
         end
         target
     end
 
-    def input_to_integer(input)
-        
+    def input_to_integer_arr(input)
+        num_arr = input.split(",").map do |el|  
+            num = el.to_i
+            num if el == "0" || num > 0
+            # if input is not a number, it will return nil for the index
+        end
+        num_arr.compact
+    end
+
+    def input_is_valid?(arr)
+        if arr.length != 2
+            p "Input Valid Target"
+            return false
+        end
+        true
     end
 end
