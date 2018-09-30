0790759d076366238d46116b65c318eb7ef80928
Steven Inouye <steveninouye@msn.com>
2018-09-29 17:39:01 -1000

    wrote necessary methods for board

--- lib/bonus/board.rb
+++ lib/bonus/board.rb
@@ -1,9 +1,15 @@
 class Board
-    def initialize(board)
-        @board = create_board(board)
-    end
+  def initialize(board)
+      @board = create_board(board)
+  end
 
-    def create_board(board)
-        Array.new(board[0]){ Array.new(board[1]) }
-    end
+  def create_board(board)
+      Array.new(board[0]){ Array.new(board[1]) }
+  end
+
+  def place_ship
+  end
+
+  def []=(coord)
+  end
 end
