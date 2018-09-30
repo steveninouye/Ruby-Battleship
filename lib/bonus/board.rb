9d645e420d61f919b7fa50f1bc3645af1f71b9f4
Steven Inouye <steveninouye@msn.com>
2018-09-29 17:36:44 -1000

    initialized board

--- lib/bonus/board.rb
+++ lib/bonus/board.rb
@@ -1,9 +1,9 @@
 class Board
-    def initialize (board)
-        @board = board
+    def initialize(board)
+        @board = create_board(board)
     end
 
     def create_board(board)
-        Array.new(board[0]){Array.new(board[1])}
+        Array.new(board[0]){ Array.new(board[1]) }
     end
 end
