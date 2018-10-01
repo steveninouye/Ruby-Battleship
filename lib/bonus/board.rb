7fdf69290d73e51a132d403f450984023a32258f
Steven Inouye <steveninouye@msn.com>
2018-09-30 18:00:55 -1000

    created method on Board to create board for ease of readibility

--- lib/bonus/board.rb
+++ lib/bonus/board.rb
@@ -1,11 +1,23 @@
 class Board
+  attr_reader :board
+
   def initialize(board)
-      @board = Array.new(board[0]){ Array.new(board[1]) }
+    @board = create_board(board)
+  end
+
+  def create_board(board)
+    rows, cols = board
+    Array.new(rows){ Array.new(cols, nil) }
   end
 
   def place_ship
   end
 
-  def []=(coord)
+  def []=(row_num, col_num, mark)
+    @board[row_num][col_num] = mark
   end
 end
+
+board = Board.new([5,6])
+board[2,2] = 1
+board.board.each {|e| p e }
