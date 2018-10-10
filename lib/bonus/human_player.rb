d2ea9be2401c921825e2dd14074b9a730d73edb3
Steven Inouye <steveninouye@msn.com>
2018-10-09 18:42:06 -0700

    moved human_player methods from the player class to human player

--- lib/bonus/human_player.rb
+++ lib/bonus/human_player.rb
@@ -7,5 +7,83 @@ class HumanPlayer < Player
     gets.chomp.split(" ")
   end
 
+  def place_ships
+    Board::SHIPS.each do |sym, obj|
+      @sym, @name, @size = sym, obj[:name], obj[:size]
+      until valid_ship_input
+        input = get_ship_input # [start_pos, direction]
+        valid_ship_input = validate_ship_input(input)
+        place_ship(valid_ship_input) if valid_ship_input
+      end
+    end
+    system "clear"
+    [:@sym, :@name, :@size, :@operator, :@start_pos, :@row_col].each {|e| remove_instance_variable(e)}
+  end
 
+  private
+
+  def get_ship_input
+    @board.full_display
+    puts "Where would you like to place your #{@name}(#{@size})?"
+    print "Starting position *space separated >2 3< => "
+    start_input = gets.chomp
+    print "Direction up(u), down(d), left(l), right(r), (retry) >u< => "
+    direction = gets.chomp
+    [start_input, direction]
+  end
+
+  def validate_ship_input(input)
+    start_input, direction = input
+    @start_pos = start_input.split.map {|e| e.to_i}
+    if @start_pos.length != 2 || "#{@start_pos[0]} #{@start_pos[1]}" != start_input
+      puts "Invalid Start Position"
+      return false
+    end
+    case direction
+      when "r"
+        @row = true
+        @operator = "+"
+        validate_placement
+      when "l"
+        @row = true
+        @operator = "-"
+        validate_placement
+      when "u"
+        @row = false
+        @operator = "-"
+        validate_placement
+      when "d"
+        @row = false
+        @operator = "+"
+        validate_placement
+      when "retry"
+        return false
+      else
+      puts "Invalid Direction"
+      return false
+    end
+    [@start_pos, direction]
+  end
+
+  def validate_placement
+    @size.times do |n|
+      if @board[operation_passer(n)] != " "
+        puts "Invalid Placement of #{@name}"
+        return false
+      end
+    end
+  end
+
+  def operation_passer(n)
+    row, col = @start_pos
+    row = row.send(@operator, n) if @row
+    col = col.send(@operator, n) if !@row
+    [row, col]
+  end
+
+  def place_ship(input)
+    @size.times do |n|
+      @board[operation_passer(n)] = @sym
+    end
+  end
 end
